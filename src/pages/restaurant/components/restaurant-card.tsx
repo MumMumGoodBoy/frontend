e9d0398a6e4ff160d8b0b02f7cb065dd0af50ab3
OakNuttawash <oak.mirthful@gmail.com>
2024-10-30 10:12:07 +0700

    feat: restaurant page

---
+++ src/pages/restaurant/components/restaurant-card.tsx
@@ -0,0 +1,32 @@
+import { Button } from '@/components/ui/button';
+import Typography from '@/components/ui/typography';
+import { Link } from 'react-router-dom';
+
+export default function ResrtaurantCard() {
+  const isAdmin = true;
+  return (
+    <div className="flex flex-col items-center justify-center p-6 max-lg:flex-col gap-4 border rounded-md shadow-sm bg-white">
+      <div className="flex justify-between w-full items-center">
+        <Link to="/food/1">
+          <Typography className="hover:underline hover:cursor-pointer">fffdddd</Typography>
+        </Link>
+      </div>
+      <img src="/src/assets/food4.svg" alt="profile" className="w-[150px] h-[150px]" />
+
+      <div className="flex flex-col gap-1 text-start w-full">
+        <Typography variant="body2" className="break-all">
+          dfsdfsfsfdsf dsf dsfdsf dsf dsfds dsf sdf dsfsdf sdf sdf ds fsd gsdgskg nslnflsen fnse nfsenf lsdnf{' '}
+        </Typography>
+      </div>
+
+      {isAdmin && (
+        <div className="flex gap-2 w-full">
+          <Button className="w-1/2" variant="secondary">
+            Edit
+          </Button>
+          <Button className="w-1/2">Delete</Button>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/pages/restaurant/index.tsx
+++ src/pages/restaurant/index.tsx
@@ -1,8 +1,36 @@
+import { Button } from '@/components/ui/button';
+import { Container } from '@/components/ui/container';
+import { Input } from '@/components/ui/input';
+import Typography from '@/components/ui/typography';
+
+import RestaurantCard from './components/restaurant-card';
+
 const Restaurants = () => {
+  const isAdmin = true;
   return (
-    <div>
-      <h1>Restaurant</h1>
-    </div>
+    <Container className="py-10 w-full">
+      <div className="flex flex-col gap-4 w-full">
+        <div className="flex justify-between">
+          <Typography variant="h2" fontWeight="bold">
+            Restaurants
+          </Typography>
+          {isAdmin && <Button variant="outline">Create restaurant</Button>}
+        </div>
+        <Input placeholder="Search food" className="w-full" />
+
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+          <RestaurantCard />
+        </div>
+      </div>
+    </Container>
   );
 };
 
